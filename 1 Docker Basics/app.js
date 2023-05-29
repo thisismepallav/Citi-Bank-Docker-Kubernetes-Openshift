192216f782be39890a87535e9ac862f023ef1ec6
Anurag <anurag.chiplunkar@gmail.com>
2023-05-29 16:03:54 +0530

    initial commit

--- 1 Docker Basics/app.js
+++ 1 Docker Basics/app.js
@@ -1,13 +1,9 @@
-import express from 'express';
-
-import connectToDatabase from './helpers.mjs'
+const express = require('express');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('<h2>Hi there!</h2>');
 });
-
-await connectToDatabase();
 
 app.listen(3000);
